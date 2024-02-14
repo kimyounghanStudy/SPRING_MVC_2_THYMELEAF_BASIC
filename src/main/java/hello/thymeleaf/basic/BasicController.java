77306bcb24ffc42b0263df859586b01b2d68c488
J-Jaeh <1wo2gur@naver.com>
2024-02-14 18:57:15 +0900

    [thymleaf-basic]utext

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -11,7 +11,12 @@ public class BasicController {
 
     @GetMapping("/text-basic")
     public String textBasic(Model model){
-        model.addAttribute("data", "Hello Spring!");
+        model.addAttribute("data", "Hello <b>Spring!</b>");
         return "basic/text-basic.html";
+    }
+    @GetMapping("/text-unescaped")
+    public String textUnescaped(Model model){
+        model.addAttribute("data", "Hello <b>Spring!</b>");
+        return "basic/text-unescaped.html";
     }
 }
